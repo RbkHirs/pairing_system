6472aa867d19ce589e089602360ad9f3fa579984
abdulazeeznaji <abdulazeeznaji@gmail>
2018-05-26 12:00:10 +0300

    Add local connections to mongoose

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,8 @@
-var mongoose = require('mongoose')
-var mongoURI = 'mongodb://admin:admin@ds129770.mlab.com:29770/paring'
+var mongoose = require('mongoose');
+var mongoURI = 'mongodb://admin:admin@ds129770.mlab.com:29770/paring';
+// var mongoURI = mongoose.connect('mongodb://localhost/pairing-system');
+
 mongoose.connect(mongoURI)
-// to active the debugger for MongoDB uncomment the line below
-// mongoose.set('debug', true);
 var db = mongoose.connection
 
 db.on('error', console.error.bind(console, 'connection error:'))
